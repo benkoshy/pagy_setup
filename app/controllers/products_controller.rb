bd83260594a88f2bfca2f8c89c17967fcc0c5292
Ben Koshy <benkoshy@hotmail.com>
2024-11-13 05:45:04 +1100

    add: set up for the pagy gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @pagy, @products = pagy(Product.all, limit: 10) 
+    # limit sets the number of items per page.
+    # you can also configure this number in the pagy's initializer
   end        
 end
